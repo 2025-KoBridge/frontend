5c78cb18fc8118c98b654f2f8f7fd5d21fa48e3d
Minji Kim <minjikim2000@hotmail.com>
2025-08-15 01:55:38 +0900

    ✨ feat: add googleLoginBtn component [KOBG-11]

---
+++ app/onboarding/login/_components/GoogleLoginBtn.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import Image from 'next/image';
+import { useState } from 'react';
+
+export default function GoogleSignInButton() {
+  const [isPressed, setIsPressed] = useState(false);
+
+  return (
+    <button
+      // Mobile
+      onTouchStart={() => setIsPressed(true)}
+      onTouchEnd={() => setIsPressed(false)}
+      // Desktop
+      onMouseDown={() => setIsPressed(true)}
+      onMouseUp={() => setIsPressed(false)}
+      onMouseLeave={() => setIsPressed(false)}
+      className={`
+        flex items-center justify-center gap-3 w-screen max-w-92 py-3 rounded-2xl
+        bg-white text-black text-bd2-bold
+        transition-all duration-150
+        ${isPressed ? 'translate-y-1' : 'shadow-gray-900 shadow-lg'}
+        hover:shadow-lg
+      `}
+    >
+      <Image
+        src="/icons/google-logo.svg"
+        alt="Google"
+        width={17.64}
+        height={18}
+      />
+      Sign in with Google
+    </button>
+  );
+}
