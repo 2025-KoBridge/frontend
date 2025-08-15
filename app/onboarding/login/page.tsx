a67f658c09c1179d9dcb6c0e2ca69dddddab7c49
Minji Kim <minjikim2000@hotmail.com>
2025-08-16 03:00:04 +0900

    ✨ feat: add login screen [KOBG-11]

--- app/onboarding/login/page.tsx
+++ app/onboarding/login/page.tsx
@@ -1,16 +1,42 @@
+'use client';
+
+import { motion } from 'framer-motion';
+import Logo from '@/components/Logo';
 import GoogleSignInButton from './_components/GoogleLoginBtn';
 
 export default function LoginPage() {
   return (
-    <div className="flex flex-col justify-between items-center">
-      <div className="flex flex-col items-center gap-2 mt-32 mb-10">
+    <div className="flex flex-col items-center  justify-center h-svh bg-gradient gap-16">
+      {/* Logo */}
+      <motion.div
+        initial={{ scale: 1, y: 144 }}
+        animate={{ scale: 0.65, y: 0 }}
+        transition={{ duration: 1, ease: 'easeInOut' }}
+        className=""
+      >
+        <Logo />
+      </motion.div>
+      {/* Text */}
+      <motion.div
+        initial={{ opacity: 0, y: 10 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.6, delay: 1.5, ease: 'easeIn' }}
+        className="flex flex-col items-center gap-2 "
+      >
         <h1 className="text-h1-bold text-black">안녕하세요, 반가워요!</h1>
         <p className="text-trans-cp1-regular text-gray-500">
           Hello, nice to meet you!
         </p>
-      </div>
-
-      <GoogleSignInButton />
+      </motion.div>
+      {/* Button */}
+      <motion.div
+        initial={{ opacity: 0, y: 0 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.6, delay: 2.4, ease: 'easeIn' }}
+        className="mt-10"
+      >
+        <GoogleSignInButton />
+      </motion.div>
     </div>
   );
 }
