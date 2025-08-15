ee7af2f70470fe7fc837bc563502946fc317e44b
Minji Kim <minjikim2000@hotmail.com>
2025-08-16 02:57:39 +0900

    🔥 remove: logo layout [KOBG-11]

--- app/onboarding/layout.tsx
+++ app/onboarding/layout.tsx
@@ -1,6 +1,3 @@
-'use client';
-
-import { usePathname } from 'next/navigation';
 import { ReactNode } from 'react';
 
 export default function OnboardingLayout({
@@ -8,16 +5,8 @@ export default function OnboardingLayout({
 }: {
   children: ReactNode;
 }) {
-  const pathname = usePathname();
-
-  const showLogoPaths = ['/onboarding', '/onboarding/login']; // 로고를 보여줄 페이지
-  const showLogo = showLogoPaths.includes(pathname);
-
   return (
     <div className="flex min-h-svh flex-col items-center bg-gradient">
-      {showLogo && (
-        <div className="mt-38 text-h1-bold text-black ">KoBridge LOGO</div>
-      )}
       {children}
     </div>
   );
