e106e84c7363e6acabe4d3eb668d960bbf29ca41
Minji Kim <minjikim2000@hotmail.com>
2025-08-16 16:50:44 +0900

    ✨ feat: add go back button to TopAppBar component [KOBG-11]

--- components/TopAppBar.tsx
+++ components/TopAppBar.tsx
@@ -1,9 +1,18 @@
+'use client';
+
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 
 export default function TopAppBar() {
+  const router = useRouter();
+
+  const handleGoBack = () => {
+    router.back();
+  };
+
   return (
     <div className="flex flex-row items-center h-14 p-4">
-      <button>
+      <button onClick={handleGoBack}>
         <Image
           src={'/icons/chevron-left.svg'}
           alt={'chevron left btn'}
