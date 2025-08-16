eb759a4c66a7b11e8a49058a7e6cec7b58908353
Minji Kim <minjikim2000@hotmail.com>
2025-08-17 00:50:40 +0900

    💄 UI: add disabled style to dropdown [KOBG-11]

--- components/Dropdown/LanguageSelect.tsx
+++ components/Dropdown/LanguageSelect.tsx
@@ -6,7 +6,11 @@ import {
 } from '@/constants/dropdown/languages';
 import { useLanguageStore } from '@/store/languageStore';
 
-export default function LanguageSelectDropdown() {
+export default function LanguageSelectDropdown({
+  disabled = false,
+}: {
+  disabled?: boolean;
+}) {
   const [isOpen, setIsOpen] = useState(false);
   const { currentLanguage, setLanguage } = useLanguageStore();
 
@@ -23,9 +27,13 @@ export default function LanguageSelectDropdown() {
       <div className="absolute inset-0 bg-gray-900 rounded-2xl translate-y-0.5" />
       {/* 드랍다운 버튼 */}
       <button
-        onClick={toggleDropdown}
-        className={`relative z-11 w-full rounded-2xl px-4 py-3 flex justify-between items-center focus:outline-none bg-white transition-colors
-          ${isOpen ? 'border-2 border-primary' : 'border border-gray-900'}`}
+        onClick={!disabled ? toggleDropdown : undefined}
+        disabled={disabled}
+        className={`relative z-11 w-full rounded-2xl px-4 py-3 flex justify-between items-center focus:outline-none transition-colors     ${
+          disabled
+            ? 'bg-gray-900 text-gray-500 cursor-not-allowed'
+            : `bg-white ${isOpen ? 'border-2 border-primary' : 'border border-gray-900'}`
+        }`}
       >
         <span className="text-trans-cp1-regular text-black gap-1 flex items-center">
           {currentLanguage.label}
@@ -33,12 +41,14 @@ export default function LanguageSelectDropdown() {
             {currentLanguage.subLabel}
           </span>
         </span>
-        <Image
-          src={isOpen ? '/icons/chevron-up.svg' : '/icons/chevron-down.svg'}
-          alt="chevron icon"
-          width={24}
-          height={24}
-        />
+        {!disabled && (
+          <Image
+            src={isOpen ? '/icons/chevron-up.svg' : '/icons/chevron-down.svg'}
+            alt="chevron icon"
+            width={24}
+            height={24}
+          />
+        )}
       </button>
       {/* 드롭다운 옵션 */}
       {isOpen && (
@@ -49,7 +59,7 @@ export default function LanguageSelectDropdown() {
               onClick={() => handleSelect(lang)}
               className="px-5 py-3 cursor-pointer text-trans-cp1-regular hover:bg-gray-100"
             >
-              {lang.label}{' '}
+              {lang.label}
               <span className="text-gray-400 text-cp1-regular">
                 {lang.subLabel}
               </span>
