2d3f201cae945362ccbf4b925829e1ec67f7c94a
Minji Kim <minjikim2000@hotmail.com>
2025-08-19 06:18:35 +0900

    💄 UI: add navigation bar component [KOBG-11]

---
+++ app/main/_components/NavigationBar.tsx
@@ -0,0 +1,54 @@
+'use client';
+
+import Image from 'next/image';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { ROUTES } from '@/constants/routes';
+
+export default function NavigationBar() {
+  const pathname = usePathname();
+
+  const navItems = [
+    {
+      label: '나의 학습',
+      href: ROUTES.MAIN.MY_LEARNING,
+      icon: '/icons/nav-book.svg',
+    },
+    {
+      label: '대화',
+      href: ROUTES.MAIN.CONVERSATION,
+      icon: '/icons/nav-phone.svg',
+    },
+    {
+      label: '마이페이지',
+      href: ROUTES.MAIN.MY_PAGE,
+      icon: '/icons/nav-user.svg',
+    },
+  ];
+
+  return (
+    <nav className="fixed bottom-0 left-0 right-0 bg-white-70 border-t border-gray-900 flex justify-around items-center h-18">
+      {navItems.map(item => {
+        const isActive = pathname === item.href;
+        return (
+          <Link
+            key={item.href}
+            href={item.href}
+            className="flex flex-col items-center justify-center gap-2"
+          >
+            <Image src={item.icon} alt={item.label} width={24} height={24} />
+            <span
+              className={
+                isActive
+                  ? 'text-secondary-400 text-cp2-bold'
+                  : 'text-gray-500 text-cp2-regular'
+              }
+            >
+              {item.label}
+            </span>
+          </Link>
+        );
+      })}
+    </nav>
+  );
+}
