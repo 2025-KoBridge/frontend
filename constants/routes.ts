15d76a9eb1699d38c2d7e560fc1d1372e8e3a0a4
Minji Kim <minjikim2000@hotmail.com>
2025-08-19 06:14:33 +0900

    ✨ feat: update route constants [KOBG-11]

--- constants/routes.ts
+++ constants/routes.ts
@@ -7,5 +7,16 @@ export const ROUTES = {
       ROOT: '/onboarding/signin',
       getStep: (step: number) => `/onboarding/signin/step${step}`,
     },
+    VOICE: {
+      ROOT: '/onboarding/voice',
+      SELECT: '/onboarding/voice/select',
+      COMPLETE: '/onboarding/voice/complete',
+    },
+  },
+  MAIN: {
+    ROOT: '/main',
+    MY_LEARNING: '/main/my-learning',
+    CONVERSATION: '/main/conversation',
+    MY_PAGE: '/main/my-page',
   },
 };
