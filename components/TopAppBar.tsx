10ac0d536405a0c332a3290af9df1ba69a6519e2
Minji Kim <minjikim2000@hotmail.com>
2025-08-19 23:33:55 +0900

    💄 UI: update to add center text [KOBG-11]

--- components/TopAppBar.tsx
+++ components/TopAppBar.tsx
@@ -3,7 +3,11 @@
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 
-export default function TopAppBar() {
+interface TopAppBarProps {
+  title?: string;
+}
+
+export default function TopAppBar({ title }: TopAppBarProps) {
   const router = useRouter();
 
   const handleGoBack = () => {
@@ -11,8 +15,9 @@ export default function TopAppBar() {
   };
 
   return (
-    <div className="flex flex-row items-center w-full my-4">
-      <button onClick={handleGoBack}>
+    <div className="relative flex flex-row items-center justify-center w-full my-4">
+      {/* 뒤로가기 버튼 */}
+      <button onClick={handleGoBack} className="absolute left-0">
         <Image
           src={'/icons/chevron-left.svg'}
           alt={'chevron left btn'}
@@ -20,6 +25,10 @@ export default function TopAppBar() {
           height={24}
         />
       </button>
+      {/* 가운데 타이틀 */}
+      {title && (
+        <span className="text-bd1-regular text-black text-center">{title}</span>
+      )}
     </div>
   );
 }
