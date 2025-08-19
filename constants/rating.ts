8b59fb98e58485715e127313479b3489f4e86ef6
Minji Kim <minjikim2000@hotmail.com>
2025-08-19 23:32:46 +0900

    ✨ feat: add step1 rating constants [KOBG-11]

---
+++ constants/rating.ts
@@ -0,0 +1,65 @@
+export const RATING_THRESHOLDS = [
+  {
+    min: 80,
+    rating: 5,
+    feedback: '발음이 아주 정확해!',
+    feedbackEn: 'Your pronunciation is perfect!',
+  },
+  {
+    min: 60,
+    rating: 4,
+    feedback: '발음이 정확해!',
+    feedbackEn: 'Your pronunciation is accurate!',
+  },
+  {
+    min: 40,
+    rating: 3,
+    feedback: '발음이 나쁘지 않은걸?',
+    feedbackEn: "Your pronunciation isn't bad?",
+  },
+  {
+    min: 20,
+    rating: 2,
+    feedback: '발음은 노력해 보자!',
+    feedbackEn: 'Keep trying to improve your pronunciation!',
+  },
+  {
+    min: 0,
+    rating: 1,
+    feedback: '발음은 많이 노력해 보자!',
+    feedbackEn: 'You need to work on your pronunciation!',
+  },
+];
+
+export const SPELLING_RATING_THRESHOLDS = [
+  {
+    min: 80,
+    rating: 5,
+    feedback: '맞춤법이 아주 정확해!',
+    feedbackEn: 'Your spelling is perfect!',
+  },
+  {
+    min: 60,
+    rating: 4,
+    feedback: '맞춤법이 정확해!',
+    feedbackEn: 'Your spelling is accurate!',
+  },
+  {
+    min: 40,
+    rating: 3,
+    feedback: '맞춤법이 나쁘지 않은걸?',
+    feedbackEn: "Your spelling isn't bad?",
+  },
+  {
+    min: 20,
+    rating: 2,
+    feedback: '맞춤법은 노력해 보자!',
+    feedbackEn: 'You should improve your spelling!',
+  },
+  {
+    min: 0,
+    rating: 1,
+    feedback: '맞춤법은 많이 노력해 보자!',
+    feedbackEn: 'You really need to work on your spelling!',
+  },
+];
