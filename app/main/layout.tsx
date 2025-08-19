1bc79d99b79765a677c42c88d659a1c9265712a8
Minji Kim <minjikim2000@hotmail.com>
2025-08-19 23:41:04 +0900

    💄 UI: add my-learning screen [KOBG-11]

--- app/main/layout.tsx
+++ app/main/layout.tsx
@@ -1,11 +1,35 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
 import { ReactNode } from 'react';
 import NavigationBar from './_components/NavigationBar';
 
-export default function MainLayout({ children }: { children: ReactNode }) {
+interface MainLayoutProps {
+  children: ReactNode;
+}
+
+export default function MainLayout({ children }: MainLayoutProps) {
+  const pathname = usePathname();
+
+  // 네비바를 보여줄 루트 경로
+  const showNavbarRoots = [
+    '/main/my-learning',
+    '/main/conversation',
+    '/main/my-page',
+  ];
+
+  // 네비바를 숨길 하위 경로 패턴
+  const hideNavbarPaths = ['/main/my-learning/level-'];
+
+  // show 조건: 루트 경로 포함 + 숨김 패턴 미포함
+  const showNavbar =
+    showNavbarRoots.some(root => pathname === root) &&
+    !hideNavbarPaths.some(pattern => pathname.startsWith(pattern));
+
   return (
     <div className="flex flex-col h-svh w-svw px-4 bg-gradient-reverse">
-      {children}
-      <NavigationBar />
+      <main className="flex-1">{children}</main>
+      {showNavbar && <NavigationBar />}
     </div>
   );
 }
--- app/main/my-learning/page.tsx
+++ app/main/my-learning/page.tsx
@@ -1,3 +1,37 @@
+'use client';
+
+import MotionFadeIn from '@/components/_animations/MotionFadeIn';
+import CharacterFrontText from './_components/CharacterFrontText';
+import LevelButton from './_components/roadmap/LevelButton';
+
 export default function MyLearningPage() {
-  return <h1>나의 학습 페이지</h1>;
+  return (
+    <>
+      <MotionFadeIn>
+        <CharacterFrontText
+          title={'첨지 오늘도 왔구나!'}
+          subtitle={'ㅇㄴㅇㄴㄴㅇ'}
+          audio
+        />
+      </MotionFadeIn>
+      <LevelButton
+        text={'학교에 가자!'}
+        subText={'Lets go to School!'}
+        status={'current'}
+        levelNum={1}
+      />
+      <LevelButton
+        text={'학교에 가자!'}
+        subText={'Lets go to School!'}
+        status={'complete'}
+        levelNum={2}
+      />
+      <LevelButton
+        text={'학교에 가자!'}
+        subText={'Lets go to School!'}
+        status={'locked'}
+        levelNum={3}
+      />
+    </>
+  );
 }
