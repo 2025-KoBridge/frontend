cb388f62fc165f7c0f54021608c6e72b0a9a561e
Minji Kim <minjikim2000@hotmail.com>
2025-08-21 07:00:42 +0900

    ✏️ typo: remove lint errors [KOBG-11]

--- app/onboarding/page.tsx
+++ app/onboarding/page.tsx
@@ -2,7 +2,7 @@ export default function OnboardingPage() {
   return (
     <div className="flex flex-col items-center gap-4">
       <h3 className="text-h1-regular">온보딩 화면</h3>
-      <p className="text-gray-600">첫 방문이면 -> 온보딩, 로그인 이력 -> main</p>
+      <p className="text-gray-600">첫 방문이면 - 온보딩, 로그인 이력 - main</p>
     </div>
   );
 }
