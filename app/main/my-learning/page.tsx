994dc43fb5c0d6fa16880b081da2e99b142d6cf6
Minji Kim <minjikim2000@hotmail.com>
2025-08-21 18:43:28 +0900

    🔥 remove: unused functions [KOBG-15]

--- app/main/my-learning/page.tsx
+++ app/main/my-learning/page.tsx
@@ -23,37 +23,6 @@ export default function MyLearningPage() {
 
   const currentLevel = 1; // 현재 레벨 예시
 
-  // 부드러운 스크롤 함수
-  const smoothScrollTo = (
-    container: HTMLElement,
-    target: number,
-    duration: number,
-  ) => {
-    const start = container.scrollTop;
-    const change = target - start;
-    let currentTime = 0;
-    const increment = 16; // 약 60fps
-
-    const animateScroll = () => {
-      currentTime += increment;
-      const val = easeInOutQuad(currentTime, start, change, duration);
-      container.scrollTop = val;
-
-      if (currentTime < duration) {
-        requestAnimationFrame(animateScroll);
-      }
-    };
-    animateScroll();
-  };
-
-  // easing 함수
-  const easeInOutQuad = (t: number, b: number, c: number, d: number) => {
-    t /= d / 2;
-    if (t < 1) return (c / 2) * t * t + b;
-    t--;
-    return (-c / 2) * (t * (t - 2) - 1) + b;
-  };
-
   //TODO: 사용자 진도로 자동 스크롤, 커스텀 훅 혹은 유틸로 리팩토링
   useEffect(() => {
     const smoothScrollTo = (
